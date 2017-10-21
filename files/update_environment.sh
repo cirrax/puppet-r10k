f0b5cc64e9a7b307513c340a453bbb6805a4047f
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2017-10-21 14:18:56 +0200

    add module update to update_environment script

--- files/update_environment.sh
+++ files/update_environment.sh
@@ -3,7 +3,6 @@
 ENV=$1
 ENV2=$2
 
-
 if [[ "$ENV" == '' ]]; then
    echo orig: $SSH_ORIGINAL_COMMAND
    ENV=`echo $SSH_ORIGINAL_COMMAND|awk -F' ' '{print $2}'`
@@ -11,20 +10,23 @@ if [[ "$ENV" == '' ]]; then
 fi
 
 if [[ ! "$ENV" =~ ^[A-Za-z0-9_]+$ ]]; then  
-   echo "$ENV not allowed, only [A-Za-z0-9_]+ allowed as environment"
+   echo "$ENV not allowed, only [A-Za-z0-9_]+ allowed as environment/module"
    exit 1
 fi
 
-if [[ ! ( "$ENV2" == '' || "$ENV2" == '--puppetfile' ) ]]; then
-   echo "only --puppetfile allowed as second argument"
-   exit 1
+if [[ "$ENV" == 'MODULE' ]]; then
+   if [[ ! "$ENV2" =~ ^[A-Za-z0-9_]+$ ]]; then  
+      echo "$ENV2 not allowed, only [A-Za-z0-9_]+ allowed as module"
+      exit 1
+   fi
+   (cd /etc/puppet; r10k --config=r10k.yaml deploy --verbose=debug module $ENV2 2>&1 |grep -E '(INFO|Updating)')
+else
+   if [[ ! ( "$ENV2" == '' || "$ENV2" == '--puppetfile' ) ]]; then
+      echo "only --puppetfile allowed as second argument"
+      exit 1
+   fi
+   if [[ $ENV == 'all' ]]; then
+      ENV=''
+   fi
+   (cd /etc/puppet; r10k --config=r10k.yaml deploy --verbose=debug environment $ENV $ENV2 2>&1 |grep -E '(INFO|Updating)')
 fi
-
-if [[ $ENV == 'all' ]]; then
-   ENV=''
-fi
-
-(cd /etc/puppet; r10k --config=r10k.yaml deploy --verbose=debug environment $ENV $ENV2 2>&1 |grep -E '(INFO|Updating)')
-
-
-#r10k --config=r10k.yaml deploy --verbose=debug module r10k 2>&1 |grep -E '(INFO|Updating)'
