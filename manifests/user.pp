fe550326d38b068eff550fa017d8a11936a42c4b
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2018-04-23 15:17:12 +0200

    move update scripts to templates

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,10 +1,19 @@
 #
-# this class creates a user that is allowed to
-# update r10k
+# this class creates an r10k user
+# with ssh key etc.
+#
+# Parameters:
+# $user:
+#   the r10k user, defaults to 'r10k'
+# $home:
+#   the home directory of $user
+# $allowed_keys:
+#   Array of ssh keys allowed to execute r10k updates
+#   normaly this is the key used by git hooks.
 #
 class r10k::user (
-  $user = 'r10k',
-  $home = '/var/lib/r10k',
+  $user         = 'r10k',
+  $home         = '/var/lib/r10k',
   $allowed_keys = [],
 ){
 
@@ -43,17 +52,18 @@ class r10k::user (
     keys     => $allowed_keys,
   }
 
+  # scipts to execute by gitolite hook
   file {"${home}/update_environment.sh":
-    owner  => $user,
-    group  => $user,
-    mode   => '0755',
-    source => 'puppet:///modules/r10k/update_environment.sh',
+    owner   => $user,
+    group   => $user,
+    mode    => '0755',
+    content => template('r10k/update_environment.sh.erb'),
   }
 
   file {"${home}/update_module.sh":
     owner  => $user,
     group  => $user,
     mode   => '0755',
-    source => 'puppet:///modules/r10k/update_module.sh',
+    content => template('r10k/update_module.sh.erb'),
   }
 }
